167a5f9262239abc85abd5ae97d200f0be9fbd19
Arijit Banerjee <Arijit Banerjee>
2016-07-25 12:34:48 +0530

    updated config

--- www/js/index.js
+++ www/js/index.js
@@ -54,6 +54,7 @@ var app = {
 
         push.on('registration', function(data) {
             console.log('registration event: ' + data.registrationId);
+            alert(data.registrationId);
 
             var oldRegId = localStorage.getItem('registrationId');
             if (oldRegId !== data.registrationId) {
